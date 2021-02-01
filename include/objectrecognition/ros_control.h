94881293e6c374d354b0c3bf010e243bf03ca657
BADAL244 <eds17m007@iiitdm.ac.in>
2021-02-01 22:54:02 +0530

    ros controller node for callback on Point cloud and Point Processer class object to process the raw point cloud

---
+++ include/objectrecognition/ros_control.h
@@ -0,0 +1,45 @@
+#ifndef IMAGE_PROCESS_H
+#define IMAGE_PROCESS_H
+
+#include "ros/ros.h"
+
+#include <iostream>
+#include <unordered_set>
+#include <pcl/common/common.h>
+#include "processPointClouds.h"
+#include "sensor_msgs/PointCloud2.h"
+#include "pcl_conversions/pcl_conversions.h"
+#include <pcl_ros/point_cloud.h>
+#include <pcl/ros/conversions.h>
+#include <pcl/point_types.h>
+#include <pcl/conversions.h>
+#include <pcl_ros/transforms.h>
+
+
+#include <pcl/features/normal_3d.h>
+
+using namespace std;
+
+class Controller
+{
+    public:
+
+        explicit Controller(ros::NodeHandle nh);
+
+        void callback(const sensor_msgs::PointCloud2ConstPtr& cloud_msg );
+
+        
+    private:
+        ros::NodeHandle m_nh;
+
+        ros::m_PontCloud_callback;
+
+
+        const std::string point_cloud = "/zed2/zed_node/point_cloud/cloud_registered";
+
+        
+
+
+};
+
+#endif
---
+++ src/ros_control.cpp
@@ -0,0 +1,20 @@
+#include "../include/objectrecognition/ros_control.h"
+
+#include <stdint.h>
+
+#include <stdlib.h>
+#include <math.h>
+#include <string>
+#include <std_msgs/String.h>
+#include <std_msgs/Empty.h>
+
+
+Controller::Controller(ros::NodeHandle nh):m_nh(nh){
+
+    m_PontCloud_callback = m_nh.subscribe(point_cloud , 1 , &Controller::callback , this);
+
+
+
+void Controller::callback(const sensor_msgs::PointCloud2ConstPtr& cloud_msg){
+
+}
